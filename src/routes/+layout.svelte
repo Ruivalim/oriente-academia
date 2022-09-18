7497753237a993273d7ca88ac2703331ac241ef2
Rui Valim <ruivalim@protonmail.com>
2022-09-18 16:30:13 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,98 @@
+<script>
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+	let navbar_open = false;
+
+	const close_navbar = () => {
+		navbar_open = false;
+	};
+</script>
+
+<nav id="nav-bar" class={navbar_open ? 'open' : ''}>
+	<button class="closebtn" on:click={close_navbar}>&times;</button>
+	<a href="/" on:click={close_navbar}>Inicio</a>
+	<a href="/kung-fu" on:click={close_navbar}>Kung Fu</a>
+	<a href="/taekwondo" on:click={close_navbar}>Taekwondo</a>
+	<a href="/tai-chi" on:click={close_navbar}>Tai Chi</a>
+	<a href="/boxe" on:click={close_navbar}>Boxe</a>
+	<a href="/blog" on:click={close_navbar}>Blog</a>
+</nav>
+
+<header>
+	<button on:click={() => (navbar_open = true)}>
+		<Fa icon={faBars} />
+	</button>
+	<a href="/"><img src="/logo.png" alt="Oriente Academia" /></a>
+	<div>
+		<a href="https://www.instagram.com/oriente.academia/" target="_blank"
+			><img width="25" height="25" src="/instagram.png" alt="Instagram Oriente Academia" /></a
+		>
+	</div>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style lang="sass">
+	#nav-bar
+		height: 100%
+		width: 250px
+		position: fixed
+		z-index: 1
+		top: 0
+		left: -250px
+		background-color: #111
+		overflow-x: hidden
+		padding-top: 60px
+		transition: 0.5s
+		&.open
+			left: 0px
+
+		// the close button stays on the top right corner and is always visible and its white and no background
+		.closebtn
+			position: absolute
+			top: 0
+			font-size: 36px
+			right: 9px
+			color: white
+			background: none
+			border: none
+
+		a
+			padding: 8px 8px 8px 32px
+			text-decoration: none
+			font-size: 25px
+			color: #818181
+			display: block
+			transition: 0.3s
+			&:hover
+				color: #f1f1f1
+
+	header
+		padding: 5px
+		width: 100%
+		position: fixed
+		top: 0px
+		background: linear-gradient(rgba(247, 148, 30, 1), rgba(244, 121, 32, 1))
+		display: flex
+		justify-content: space-between
+		align-items: center
+
+		button
+			background: none
+			border: none
+			font-size: 25px
+			cursor: pointer
+			color: white
+			display: inline-block
+
+		div	
+			margin-right: 10px
+		
+
+	main
+		margin-top: 75px
+		padding: 5px
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<h1>Oriente Academia</h1>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, dicta laboriosam perspiciatis
+	voluptate non quia cumque sint maxime laborum? Illo animi qui numquam molestias ipsa molestiae
+	velit iste necessitatibus dignissimos deserunt in, architecto, officiis assumenda autem maiores
+	accusantium veniam, esse obcaecati! Mollitia ratione officiis earum repudiandae, neque adipisci
+	alias autem nemo quibusdam, omnis, odio esse illo voluptatibus? Obcaecati iure quis asperiores sed
+	dolorum sapiente in qui impedit aliquam laborum ea non culpa, animi architecto magni cum corrupti
+	consequuntur ipsum ipsa ut ab officiis. Iste autem aliquam nihil odio repudiandae facere mollitia
+	saepe blanditiis repellendus consectetur minus obcaecati amet rerum voluptas porro totam velit
+	iure, cupiditate ex laborum ducimus nostrum. Repudiandae fugit similique aliquam magni ratione
+	consequatur odio obcaecati in ducimus error laborum, tenetur repellendus doloribus ullam. Earum
+	reprehenderit itaque cumque? Nobis eum hic dolore dignissimos numquam! Necessitatibus, facere!
+	Nemo blanditiis sapiente debitis, placeat asperiores fugit, iste atque animi odit quidem doloribus
+	inventore eos magnam vel a quia odio at aliquam ullam voluptate amet distinctio molestias! Veniam,
+	ullam quasi atque ipsa id ipsum aperiam natus? Accusantium aliquid consequuntur officia reiciendis
+	voluptatem tenetur rerum illo. Quisquam aspernatur eum nemo porro magnam. Modi repellendus
+	reiciendis qui sint repudiandae mollitia nobis, ipsam reprehenderit fuga iusto sit ut aliquid
+	magni officia sunt quibusdam asperiores suscipit aspernatur quam aperiam tenetur error, nemo
+	perspiciatis illo! Voluptatem vitae qui quia, hic ea dignissimos ratione sit debitis nobis eaque
+	quaerat architecto libero iste culpa commodi maxime praesentium est voluptates, aliquam minima et
+	harum! Est doloribus blanditiis pariatur, labore, officia suscipit vero dolores ullam cumque velit
+	voluptatem minus dolore natus veniam tempora nam nisi dignissimos? Consequatur quos delectus
+	aliquid sequi ex in earum consectetur obcaecati molestiae commodi expedita deserunt iure repellat
+	porro quasi maxime excepturi asperiores atque tenetur eligendi cupiditate assumenda, debitis
+	voluptate quas. Explicabo velit dolorum vel. Maxime at eos neque placeat beatae dolorum corporis
+	quam ea ratione mollitia harum sint cupiditate laborum debitis aliquid incidunt totam labore sequi
+	sapiente optio, recusandae velit reiciendis aliquam ipsam. At magni omnis odit laboriosam
+	accusantium et illo dicta maiores molestiae reiciendis odio, facere asperiores deserunt tempore, a
+	ab repudiandae officia nobis impedit quas ex! Doloremque, minus, ea, beatae expedita aliquid alias
+	iste harum sequi explicabo numquam dolore voluptatum consequuntur quaerat nihil incidunt
+	temporibus quas culpa. Nemo hic obcaecati error tempore vitae repudiandae, ullam dolorum ipsum non
+	ipsa ipsam, facilis doloribus expedita alias soluta! Distinctio libero nesciunt aspernatur ullam
+	eaque non maxime at, quae maiores eius iusto dolorum quo aliquam. Aliquam, ut. Odit, neque ipsum
+	quae alias saepe hic quas quidem iure maxime, expedita odio veritatis quisquam magni laborum ullam
+	fugit natus. Quam animi iusto minima at dolorum labore dolor maxime expedita vitae recusandae
+	nostrum rerum sequi iure cum exercitationem, quae, sunt, dignissimos officia saepe facere et fuga.
+	Debitis, in odit. Consequatur, omnis reiciendis, atque nisi laboriosam dicta expedita quaerat
+	porro aspernatur adipisci perspiciatis in sequi ea doloribus quibusdam hic dolorum similique
+	possimus. Facilis perspiciatis et aperiam dicta, quis architecto qui eveniet delectus harum at
+	quas ipsum voluptate velit est praesentium modi sed accusamus inventore iure odit provident?
+	Dolorum ad quidem iste provident nisi libero dignissimos ullam magni soluta rerum tempora,
+	voluptatem, accusamus facere odio quo excepturi harum vitae repudiandae unde vero qui debitis.
+	Expedita eius, nemo ducimus aspernatur esse rem qui doloribus reiciendis optio perferendis error
+	distinctio culpa necessitatibus mollitia ab vel itaque quo dolor totam aut iste deserunt omnis?
+	Soluta error facilis expedita eius earum impedit temporibus qui inventore et doloribus at labore
+	rem, repellat odio aliquam? Cupiditate quam illum rem dicta aliquid magni? Excepturi est accusamus
+	voluptatum vitae aperiam odio blanditiis consequatur nulla placeat cumque consequuntur aliquam, in
+	earum neque quo facilis dolor hic, porro quam alias perferendis esse aliquid illo. Recusandae fuga
+	necessitatibus dolor quod nemo quis eius officiis dolore molestias mollitia dolores, illum ea
+	ipsum esse et? Fugiat quo iusto minus pariatur quisquam beatae iste asperiores. Incidunt illum
+	dolor voluptatum. Qui blanditiis facilis officiis at neque aperiam nostrum, recusandae, delectus
+	sequi voluptate voluptas, animi ea! Ab reiciendis sed possimus nihil amet est a praesentium
+	excepturi totam natus. Quasi voluptatibus ut officia illo esse est expedita aliquam eos explicabo
+	at animi ipsum asperiores fugiat, assumenda itaque. Tenetur officiis consequuntur tempore animi,
+	quasi a dolorum explicabo nihil ab voluptas, laboriosam neque atque obcaecati error, quis rerum
+	quidem porro illo praesentium nostrum? Laboriosam dolorem voluptatem incidunt corporis, odit nulla
+	eum, assumenda fuga enim, quaerat adipisci. Nemo similique molestias expedita ad ullam ducimus
+	numquam eaque ipsum accusantium quae adipisci, magni porro voluptatibus quod exercitationem,
+	aperiam quaerat inventore modi deserunt ex? Repudiandae, est ducimus. Corporis provident facere
+	eaque nemo quam voluptatem quisquam quis iure doloremque aliquam cumque aspernatur ipsum
+	accusamus, impedit veniam sint minima. Libero, eum perferendis facilis explicabo veritatis
+	assumenda voluptatem esse hic! Atque cum perferendis ducimus unde quis vitae impedit maxime
+	blanditiis fugit harum, aut voluptates possimus quos iusto numquam obcaecati repudiandae fugiat
+	molestias est cupiditate hic id qui. Temporibus placeat magni eius aperiam recusandae modi,
+	blanditiis perspiciatis doloribus eos! Architecto illo repellat animi consectetur placeat rem
+	voluptates sunt molestiae fuga ipsam! Consequatur mollitia autem tempora omnis porro. Accusantium
+	modi inventore aliquam molestias aspernatur voluptates facilis, ea facere? Est fuga laudantium
+	assumenda cupiditate animi, quisquam nesciunt quod eum ullam libero, incidunt, dolores corporis
+	iure pariatur. Possimus voluptatum nisi dolorem sapiente quis? Odit soluta assumenda iure suscipit
+	quis, natus sequi quod aperiam maiores et dicta necessitatibus quasi labore iusto autem, quibusdam
+	placeat veniam pariatur voluptatem repudiandae totam eaque, consectetur minus. Blanditiis eius
+	eveniet enim itaque impedit natus a porro, architecto sed, saepe rerum mollitia ipsa veniam
+	explicabo dolorem? Aliquid officia nam esse natus. Maiores repellendus, eos voluptatem atque vel
+	quia exercitationem cum blanditiis voluptas amet debitis odio sint, in perspiciatis vitae iusto
+	est, nobis suscipit eligendi aliquid omnis. Minus, iure eum? Quia veniam porro error.
+	Reprehenderit dignissimos beatae enim qui corporis ab sit vel modi distinctio molestias temporibus
+	ratione veritatis saepe vero, nemo voluptatem, magnam facere, ullam hic mollitia. Fugiat,
+	consequatur explicabo magni maiores beatae repellendus corporis vel quaerat, temporibus asperiores
+	quae tenetur quam corrupti amet sunt sequi. Culpa soluta veniam deleniti.
+</p>
